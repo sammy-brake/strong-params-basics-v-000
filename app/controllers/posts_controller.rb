e6ecd38a5ad1095204b49fc5cb9ae3ba7a60a1bd
Samantha brake <sammy.brake@gmail.com>
2019-04-04 22:55:20 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,14 +11,14 @@ class PostsController < ApplicationController
 		@post = Post.new
 	end
 
-	
+
 
 	def create
   @post = Post.new(params.require(:post).permit(:title, :description))
   @post.save
   redirect_to post_path(@post)
 	end
- 
+
 def update
   @post = Post.find(params[:id])
   @post.update(params.require(:post).permit(:title))
